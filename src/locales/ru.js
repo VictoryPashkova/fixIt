01849a90525b1dc4cd03f96da0758040123796df
VictoryPashkova <victory.pashkova@gmail.com>
2024-06-25 16:54:21 +0600

    feat: initialize application and create initial HTML structure

---
+++ src/locales/ru.js
@@ -0,0 +1,13 @@
+const ru = {
+  translation: {
+    errorInvalidUrl: 'Ссылка должна быть валидным URL',
+    errorDoubUrl: 'RSS уже существует',
+    successLoadedRSS: 'RSS успешно загружен',
+    postTitle: 'Посты',
+    feedTitle: 'Фиды',
+    viewButtonText: 'Просмотр',
+    networkErr: 'Ошибка сети',
+    RSSerr: 'Ресурс не содержит валидный RSS',
+  },
+};
+export default ru;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/images/[name][ext]',
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: isProduction ? 'production' : 'development',
+};
+
+module.exports = config;
